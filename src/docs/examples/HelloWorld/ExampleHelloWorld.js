0c8cf0c161621fcfcdb5de2d4861eaefbeff0087
Lucas Grimauth <lgevan@br.ibm.com>
2017-10-31 16:39:55 -0200

    Documentation, Atoms and Molecules

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+
+/** Simple example */
+function ExampleHelloWorld() {
+	return <HelloWorld message="viewers!" />
+};
+
+export default ExampleHelloWorld;
